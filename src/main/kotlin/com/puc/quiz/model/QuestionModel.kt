cccb0b44c3770599aeefb335254d9d82b91e52d3
AndreDLeite <andre.durte@hotmail.com>
2020-06-16 09:22:35 -0300

    start commit for server site of puc-quiz

---
+++ src/main/kotlin/com/puc/quiz/model/QuestionModel.kt
@@ -0,0 +1,14 @@
+package com.puc.quiz.model
+
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+
+@Entity
+data class QuestionItem(var name: String = "",
+                        var isAvailable: Boolean = false,
+                        @Id
+                        @GeneratedValue(strategy = GenerationType.AUTO)
+                        var id: Long = 0)
+
---
+++ src/main/kotlin/com/puc/quiz/repository/QuestionItemRepository.kt
@@ -0,0 +1,5 @@
+package com.puc.quiz.repository
+
+import org.springframework.stereotype.Repository
+
+@Repository
