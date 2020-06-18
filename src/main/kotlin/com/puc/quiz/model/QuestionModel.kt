f60bd45cef356a1c9aa20124793b3d73d49f79c8
AndreDLeite <andre.durte@hotmail.com>
2020-06-17 21:27:38 -0300

    fixing the question object model

--- src/main/kotlin/com/puc/quiz/model/QuestionModel.kt
+++ src/main/kotlin/com/puc/quiz/model/QuestionModel.kt
@@ -1,21 +1,18 @@
 package com.puc.quiz.model
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import javax.persistence.*
 
-@Entity()
-data class QuestionItem(var name: String = "",
+@Entity
+data class QuestionItem(@Id
+                        @GeneratedValue(strategy = GenerationType.AUTO)
+                        var id: Long = 0,
+                        var name: String = "",
                         var isAvailable: Boolean = false,
                         var question: String = "",
+                        @ElementCollection
                         var options: List<String>,
                         var correctOption: Int,
-                        var questionType: QuestionType = QuestionType.UNKNOWN,
-                        @Id
-                        @GeneratedValue(strategy = GenerationType.AUTO)
-                        var id: Long = 0)
-{
+                        var questionType: QuestionType = QuestionType.UNKNOWN) {
     fun isValid(): Boolean = options.size > 3
 }
 
